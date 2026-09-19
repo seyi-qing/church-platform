efbb060245db7b70f07d4acbaaf6adbcbafbb5c4
Seyi Qing <logzmaster.biz@gmail.com>
2026-09-19 15:40:14 +0300

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,5 @@
+# app/models/__init__.py
+from app.db.base import Base
 from app.models.user import User
 from app.models.member import MemberProfile, Family, Group, GroupMembership, Attendance
 from app.models.giving import Donation, RecurringDonation
